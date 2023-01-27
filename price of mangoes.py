3415fba2a0ba4e4c012eb68aa2c46b2a3612637b
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-27 23:53:14 +0500

    Update price of mangoes.py

--- price of mangoes.py
+++ price of mangoes.py
@@ -1,1 +1,19 @@
+mangoes = int(input("Please Enter Number Of Mangoes You Want to Buy:  "))
+price = int(input("Please Enter The Price of one Mango: $ "))
+priced = 0
+free = 0
 
+while mangoes > 0:
+    if mangoes > 2:
+        mangoes -= 2
+        priced += 2
+        mangoes -= 1
+        free += 1
+    else:
+        priced += mangoes
+        mangoes -= mangoes
+
+
+bill = price * priced
+
+print(f"You Have got {free} Free Mangoes. & your bill is ${bill}")
