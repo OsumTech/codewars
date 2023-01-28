e3abe8b2b613e108a756790dcef253ee0aa65753
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-29 00:02:10 +0500

    Update Fibonacci Number.py

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -2,12 +2,15 @@ sum_fibonacci = 2
 fibonacci = [0, 1]
 first_fibonacci = 0
 second_fibonacci = 1
-
-while not sum_fibonacci >= 4000:
+limit = True
+while limit:
     current_fibonacci = first_fibonacci + second_fibonacci
     fibonacci.append(current_fibonacci)
     if current_fibonacci % 2 == 0:
         sum_fibonacci += current_fibonacci
+    if sum_fibonacci >= 4000:
+        sum_fibonacci -= current_fibonacci
+        limit = False
     first_fibonacci = second_fibonacci
     second_fibonacci = current_fibonacci
 
