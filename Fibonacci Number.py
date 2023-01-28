fbc910c6073a354caad2f0378f745599bada2557
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-28 22:32:14 +0500

    Update Fibonacci Number.py

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -1,1 +1,15 @@
+even_number = 0
+fibonacci = 0
+sum_fibonacci =0
+number = 0
+even_fibonacci = []
+while not fibonacci >= 400:
+    fibonacci += number
+    number +=1
+    if fibonacci % 2 == 0:
+        sum_fibonacci += fibonacci
+        even_fibonacci.append(fibonacci)
+        
 
+print(even_fibonacci)
+print(sum_fibonacci)
