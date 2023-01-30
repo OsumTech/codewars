ffc1de6cae72ee3e16e23592759a0b045927846a
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-30 22:00:19 +0500

    Create Largest Prime Factor.py

--- Largest Prime Factor.py
+++ Largest Prime Factor.py
@@ -1,2 +1,15 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
+number = int(input("Please Enter the number you want to Check: "))
+divisor = 1
+all_divisors = []
+for divisor in range (1,11):
+    answer = number % divisor
+    if answer == 0:
+        all_divisors.append(divisor)
+    divisor += 1
+
+
+print(all_divisors)
+
+
