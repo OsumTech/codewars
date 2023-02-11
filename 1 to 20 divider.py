45de5ad8431aae0c0c47cda60b3ad455071a6797
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-02-11 23:09:56 +0500

    Update 1 to 20 divider.py

--- 1 to 20 divider.py
+++ 1 to 20 divider.py
@@ -1,1 +1,12 @@
 
+number = 2520
+is_number = False
+while not is_number:
+    for i in range(1,20):
+        if number % i == 0:
+            is_number = True
+        else:
+            is_number = False
+
+    if not is_number:
+        number += number
